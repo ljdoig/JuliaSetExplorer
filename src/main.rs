498f1d0e617b4c436699d47140e38f22fa3771c3
ldoig <lachjd@protonmail.com>
2023-05-18 12:17:26 +1000

    Julia explorer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use image::Rgb;
 use minifb::{Key, KeyRepeat, MouseButton, MouseMode, Window, WindowOptions};
 use rayon::prelude::*;
 use std::time::{Duration, Instant};
+
+mod josh_palette;
+use josh_palette::ColorPalette;
 
 const WIDTH: usize = 900;
 const HEIGHT: usize = 600;
@@ -9,29 +13,11 @@ const HEIGHT_F: f64 = HEIGHT as f64;
 
 const MAX_ITERATION_JUMP: u32 = 250;
 const MAX_ITERATION_LOWER_BOUND: u32 = 100;
-const ZOOM_FACTOR: f64 = 1.1;
+const SCROLL_ZOOM: f64 = 1.1;
+const KEYS_ZOOM: f64 = 2.0;
+const KEYS_TRANSLATION: f64 = 0.01;
 
 const CLICK_DELAY_MILLIS: u64 = 150;
-
-const COLOURS: [(u8, u8, u8); 4] = [
-    (10, 147, 150),  // Dark blue
-    (0, 18, 25),     // Blackish
-    (174, 32, 18),   // Orange
-    (233, 216, 166), // Light yellow
-];
-const NUM_COLOURS: usize = COLOURS.len();
-
-fn linear_interpolation((r1, g1, b1): (u8, u8, u8), (r2, g2, b2): (u8, u8, u8), t: f64) -> u32 {
-    let red = interpolate_component(r1, r2, t);
-    let green = interpolate_component(g1, g2, t);
-    let blue = interpolate_component(b1, b2, t);
-    red << 16 | green << 8 | blue
-}
-
-fn interpolate_component(c1: u8, c2: u8, t: f64) -> u32 {
-    let interpolated = (c1 as f64) * (1.0 - t) + (c2 as f64) * t;
-    interpolated.round() as u32
-}
 
 #[derive(PartialEq, PartialOrd, Clone, Debug)]
 struct Params {
@@ -47,7 +33,7 @@ impl Params {
     fn new() -> Self {
         Params {
             zoom: 1.0,
-            centre_x: -1.0,
+            centre_x: 0.0,
             centre_y: 0.0,
             max_iterations: 100,
             scroll: 0.0,
@@ -55,9 +41,9 @@ impl Params {
         }
     }
 
-    fn get_iterations(&self, c_re: f64, c_im: f64) -> f64 {
-        let mut z_re = 0.0;
-        let mut z_im = 0.0;
+    fn get_iterations(&self, mut z_re: f64, mut z_im: f64) -> f64 {
+        let c_re = self.centre_x;
+        let c_im = self.centre_y;
         let mut iterations = 0;
         while iterations < self.max_iterations && z_re * z_re + z_im * z_im <= 4.0 {
             let z_re_ = z_re;
@@ -72,18 +58,6 @@ impl Params {
         }
     }
 
-    fn colour_iterations(&self, mut iterations: f64) -> u32 {
-        iterations *= 0.03;
-        let iter_whole = iterations.floor();
-        let iter_frac = iterations - iter_whole;
-        let iter_whole = iter_whole as usize;
-
-        let colour = COLOURS[iter_whole % NUM_COLOURS];
-        let next_colour = COLOURS[(iter_whole + 1) % NUM_COLOURS];
-
-        linear_interpolation(colour, next_colour, iter_frac)
-    }
-
     fn region_width(&self) -> f64 {
         4.0 / self.zoom
     }
@@ -92,12 +66,12 @@ impl Params {
         self.region_width() * HEIGHT_F / WIDTH_F
     }
 
-    fn get_pixels(&self) -> Vec<u32> {
+    fn get_pixels(&self, palette: &ColorPalette) -> Vec<u32> {
         let start_time = Instant::now();
         let precomp1 = self.region_width() / WIDTH_F;
-        let precomp2 = -self.region_width() / 2.0 + self.centre_x;
+        let precomp2 = -self.region_width() / 2.0; // + self.centre_x;
         let precomp3 = -self.region_height() / HEIGHT_F;
-        let precomp4 = self.region_height() / 2.0 + self.centre_y;
+        let precomp4 = self.region_height() / 2.0; //+ self.centre_y;
         let output = (0..HEIGHT * WIDTH)
             .into_par_iter()
             .map(|i| {
@@ -107,7 +81,7 @@ impl Params {
                 let y = row as f64 * precomp3 + precomp4;
                 self.get_iterations(x, y)
             })
-            .map(|iterations| self.colour_iterations(iterations))
+            .map(|iterations| palette.value(iterations / self.max_iterations as f64))
             .collect();
         println!(
             "{:?} for: Max iters = {}, Zoom = {}, Centre = ({},{})",
@@ -136,27 +110,27 @@ impl Params {
             }
         }
         if window.is_key_pressed(Key::Up, KeyRepeat::Yes) {
-            self.centre_y += 0.1 * region_height; // Pan up
+            self.centre_y += KEYS_TRANSLATION * region_height; // Pan up
         }
         if window.is_key_pressed(Key::Down, KeyRepeat::Yes) {
-            self.centre_y -= 0.1 * region_height; // Pan down
+            self.centre_y -= KEYS_TRANSLATION * region_height; // Pan down
         }
         if window.is_key_pressed(Key::Right, KeyRepeat::Yes) {
-            self.centre_x += 0.1 * region_width; // Pan right
+            self.centre_x += KEYS_TRANSLATION * region_width; // Pan right
         }
         if window.is_key_pressed(Key::Left, KeyRepeat::Yes) {
-            self.centre_x -= 0.1 * region_width; // Pan left
+            self.centre_x -= KEYS_TRANSLATION * region_width; // Pan left
         }
 
         // Zooming
         if window.is_key_pressed(Key::W, KeyRepeat::Yes) {
-            self.zoom *= 2.0;
+            self.zoom *= KEYS_ZOOM;
         }
         if window.is_key_pressed(Key::S, KeyRepeat::Yes) {
-            self.zoom *= 0.5;
+            self.zoom *= 1.0 / KEYS_ZOOM;
         }
         if let Some((_, scroll)) = window.get_scroll_wheel() {
-            self.zoom *= ZOOM_FACTOR.powf(scroll as f64);
+            self.zoom *= SCROLL_ZOOM.powf(scroll as f64);
         }
 
         // Changing max iterations
@@ -182,13 +156,28 @@ impl Params {
 struct State {
     params: Params,
     pixels: Vec<u32>,
+    palette: ColorPalette,
 }
 
 impl State {
     fn new() -> Self {
+        let palette = ColorPalette::new(vec![
+            (0., Rgb([0, 18, 25])),
+            (0.05, Rgb([0, 18, 25])),
+            (0.2, Rgb([20, 33, 61])),
+            (0.35, Rgb([252, 163, 17])),
+            (0.4, Rgb([229, 229, 229])),
+            (0.6, Rgb([255, 255, 255])),
+            (1., Rgb([0, 0, 0])),
+        ])
+        .unwrap();
         let params = Params::new();
-        let pixels = params.get_pixels();
-        State { params, pixels }
+        let pixels = params.get_pixels(&palette);
+        State {
+            params,
+            pixels,
+            palette,
+        }
     }
 
     fn update(&mut self, window: &Window) {
@@ -198,7 +187,7 @@ impl State {
 
         // Only update pixels if the parameters have been changed
         if self.params != old_params {
-            self.pixels = self.params.get_pixels();
+            self.pixels = self.params.get_pixels(&self.palette);
         }
     }
 }
